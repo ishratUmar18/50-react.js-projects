2245c8678ef3c26c42850d8e6bb16ce9213e7b13
Ishrat <47534248+ishratUmar18@users.noreply.github.com>
2022-08-01 00:13:22 +0500

    Update App.jsx

--- AirBnB Clone/src/App.jsx
+++ AirBnB Clone/src/App.jsx
@@ -2,14 +2,24 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import HeroSection from "./components/Hero-section"
 import Card from "./components/Card"
-
+import data from "./data"
 
 export default function App() {
-    return (
-        <div>
-            <Navbar />
-            <HeroSection />
-            <Card />
-        </div>
-    )
-}
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+            />
+        )
+    })    
+
+return (
+<div>
+    <Navbar />
+    <HeroSection />
+    <section className="cards-list">
+      {cards}
+    </section>
+</div>
+)}
