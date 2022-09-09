1e9701503be42fd73d8ea4f754c393742cb2cbdd
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-09-09 11:09:41 +0500

    add static page

---
+++ static-page/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return(
+        <nav>
+        <img src="https://img.icons8.com/color/344/react-native.png" className="nav-icon"/>
+        <h3 className="nav-logo-text">ReactFacts</h3>
+        <h4 className="nav-title">React Course - Project 1</h4>
+        </nav>
+)
+}
