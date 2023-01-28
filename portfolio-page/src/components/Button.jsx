10a29833b2f854735867a2d155eb249db9ce5465
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2023-01-28 21:08:46 +0500

    add buttons

--- portfolio-page/src/components/Button.jsx
+++ portfolio-page/src/components/Button.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <Button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
-     Get Started
-    </Button>
+    <div className="flex space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-12">
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[8px]`}>
+     Demo
+    </button>
+    <button class={`py-4 px-6 bg-transparent hover:bg-blue-gradient font-poppins font-medium text-[18px] text-primary text-gradient border border-blue-gradient ${styles} rounded-[8px]`}>
+  Code
+</button>
+    </div>
   )
 }
-
 export default Button
--- portfolio-page/src/components/Projects.jsx
+++ portfolio-page/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import { project_01, apple, google } from '../assets';
 import styles, { layout } from '../style';
-// import Button from './Button';
+import Button from './Button';
 
 const Projects = () => (
   <>
@@ -10,7 +10,7 @@ const Projects = () => (
         <h2 className={styles.heading2}>Some of my projects</h2>
         <h4 className={styles.heading4}> Landing Page</h4>
         <p className={ `${styles.paragraph} max-w-[470px] mt-5`}>Cras ultricies ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-        {/* <Button styles="mt-10" />  */}
+        <Button styles="mt-10" /> 
       </div>
       <div className={layout.sectionImg}>
         <img src={project_01} alt="card" className="w-[100%] h-[100%]" />
