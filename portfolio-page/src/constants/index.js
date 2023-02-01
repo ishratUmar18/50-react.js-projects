50e02de252de63ac9c08eb0cdd665ac5b980b7f8
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2023-02-01 21:39:24 +0500

    update footer

--- portfolio-page/src/constants/index.js
+++ portfolio-page/src/constants/index.js
@@ -1,3 +1,5 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
 export const navLinks = [
   {
     id: "about",
@@ -14,6 +16,56 @@ export const navLinks = [
   {
     id: "contact",
     title: "Contact",
+  },
+];
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
   },
 ];
 export const stats = [
@@ -39,23 +91,23 @@ export const footerLinks = [
     links: [
       {
         name: "Content",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/content/",
       },
       {
         name: "How it Works",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/how-it-works/",
       },
       {
         name: "Create",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/create/",
       },
       {
         name: "Explore",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/explore/",
       },
       {
         name: "Terms & Services",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/terms-and-services/",
       },
     ],
   },
@@ -64,23 +116,23 @@ export const footerLinks = [
     links: [
       {
         name: "Help Center",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/help-center/",
       },
       {
         name: "Partners",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/partners/",
       },
       {
         name: "Suggestions",
-        link: "https://twitter.com/ishratUmar18",
+        link: "https://www.portfolio.com/suggestions/",
       },
       {
         name: "Blog",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/blog/",
       },
       {
         name: "Newsletters",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/newsletters/",
       },
     ],
   },
@@ -89,12 +141,52 @@ export const footerLinks = [
     links: [
       {
         name: "Our Partner",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/our-partner/",
       },
       {
         name: "Become a Partner",
-        link: "https://ishratumar.medium.com/",
+        link: "https://www.portfolio.com/become-a-partner/",
       },
     ],
+  },
+];
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
   },
 ];
